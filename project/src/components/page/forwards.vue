76f931d7ad2d4b8cce600792b44ceb7353c24fa0
magxyu <magxyu@mit.edu>
2023-09-16 12:04:22 -0400

    main page setup

---
+++ project/src/components/page/forwards.vue
@@ -0,0 +1,11 @@
+<template>
+    <button>
+        Next
+    </button>
+</template>
+
+<script>
+export default{
+    name: 'forwards'
+}
+</script>
---
+++ project/src/components/page/page.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Here is a child component!</h1>
+  <forwards />
+</template>
+
+
+<script>
+import forwards from './forwards.vue'
+
+export default {
+  name: "header",
+  components: {
+    forwards
+  }
+}
+
+</script>
+

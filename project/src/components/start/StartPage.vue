a2e62e799dbc47fd97f559fd4c4626730993eb8e
Erin Liu <erinliu@mit.edu>
2023-09-16 17:40:14 -0400

    edited start page

--- project/src/components/start/StartPage.vue
+++ project/src/components/start/StartPage.vue
@@ -1,37 +1,30 @@
 <script setup>
 import Button from './StartButton.vue'
+import Title from './Title.vue'
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="../icons/logo.svg" width="125" height="125" />
-
-        <!-- <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-        </div> -->
-    </header>
-    <main>
-        <Button />
-    </main>
+    <div class="main">
+        <div class="block">
+            <img alt="Vue logo" class="logo" src="../icons/logo.png" width="400" height="400" />
+        </div>
+        <div class="block">
+            <Title />
+            <Button />
+        </div>
+    </div>
 </template>
 
 <style scoped>
-body {
-    background: #ffecd9;
-    display: grid;
-    height: 100vh;
-    margin: 0;
+.main {
+    display: flex;
+    flex-direction: row;
+}
+.block {
+    display: flex;
+    flex-direction: column;
     place-items: center;
-    padding: 1rem;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+    flex-wrap: wrap;
 }
 
 @media (min-width: 1024px) {
---
+++ project/src/components/start/Subtitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <h3>Embark on a journey into the world of Chinese literature.</h3>
+</template>
+
+<style scoped>
+
+</style>
